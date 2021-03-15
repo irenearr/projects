c96346f151b2bc652c7045f8c57cc2e47cac4979
irenevandijk <irene.van.dijk@mediamonks.com>
2021-03-15 21:43:53 +0100

    add registration app

---
+++ multi-step-form-using-mern/src/router/AppRouter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import FirstStep from '../components/FirstSteps';
+import Header from '../components/Header';
+
+const AppRouter = () => {
+
+    return(
+       <BrowserRouter>
+    <div className="container">
+        <Header />
+        <Switch>
+            <Route component={FirstStep} path="/" exact={true} />
+        </Switch>
+    </div>
+</BrowserRouter>
+    )
+    }
+
+export default AppRouter;
